b18ef23515aea93a715bcee1286c894a0829d030
HunnidStax <den4ik.trofimenko@gmail.com>
2021-12-06 17:20:39 +0300

    4_1

---
+++ Les4_4/Les4_4/House.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Les4_4
+{
+    class House
+    {
+        public int porchCount;
+        public int floorCount;
+        private int roomsOnFloor;
+        private static int numhouse = 0;
+
+        public int Porchs
+        {
+            get { return porchCount; }
+            set { porchCount = value; }
+        }
+
+        public int Floors
+        {
+            get { return floorCount; }
+            set { floorCount = value; }
+        }
+
+        public int RoomsOnFloor
+        {
+            get { return roomsOnFloor; }
+            set { roomsOnFloor = value; }
+        }
+
+        public static int NumHouse{ get; set; }
+        public void SetNumHouse()
+        {
+            numhouse += 1;
+        }
+        public House()
+        {
+            SetNumHouse();
+        }
+
+        public House(int porchCount, int floorCount, int roomsOnFloor) : this()
+        {
+            this.porchCount = porchCount;
+            this.floorCount = floorCount;
+            this.roomsOnFloor = roomsOnFloor;
+        }
+
+        public int GetRoomsCountInPorch()
+        {
+            return roomsOnFloor * floorCount;
+        }
+
+        public int GetRoomsCountInHouse()
+        {
+            return GetRoomsCountInPorch() * porchCount;
+        }
+
+
+        public override string ToString()
+        {
+            return string.Format("Количество этажей: {0} \nКоличество подъездов: {1} \nНомер дома: {2}", Floors, Porchs, numhouse);
+        }
+    }
+}
