5d9f334ae779df4088d2a67dc70bd6a7d5b298fe
HunnidStax <den4ik.trofimenko@gmail.com>
2021-12-13 06:34:56 +0300

    Done

---
+++ Les_5.1/Les_5.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Les_5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RationalNumber rationalNumber = new RationalNumber(1, 2);
+            Console.WriteLine(rationalNumber.ToString());
+
+            RationalNumber sum = rationalNumber + rationalNumber;
+            Console.WriteLine(sum.ToString());
+
+            Console.WriteLine(rationalNumber.Equals(rationalNumber));
+        }
+    }
+}
